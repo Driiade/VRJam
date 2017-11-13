328417ef9539503748196c2c101a1e6a708f8788
CONSTANTIN <benoitc@equilibregames.com>
2017-11-13 11:05:16 +0100

    -ADD : project with plug-in

---
+++ VRJam/VRJam/Assets/Resources/GlobalReprojectionPass.shader
@@ -0,0 +1,90 @@
+﻿Shader "Custom/GlobalReprojectionPass"
+{
+	Properties
+	{
+		_ReprojectionMaskColor("Reprojection Mask Color", Color) = (1,1,1,1)
+	}
+	SubShader
+	{
+		Cull Off
+		ZWrite On
+		ZTest Always
+
+		Pass
+		{
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+
+			#define _DepthThreshold    0.001f
+			float4x4 _OtherEyeViewProj;
+			float4 _ReprojectionMaskColor;
+			UNITY_DECLARE_TEX2D(_OtherEyeTex);
+			UNITY_DECLARE_TEX2D(_OtherEyeDepthTexture);
+			sampler2D _CameraDepthTexture;
+			float4x4 _InvViewProj;
+			float _ShowReprojected;
+
+			// Sample a triangle shape and found the closet depth to the eye
+			// Note: Unity 5.6 is using inversed depth buffer here, so the bigger value , the closer
+			float GetOtherEyeConservativeDepth(float2 otherEyeUV)
+			{
+				float otherEyeDepth = _OtherEyeDepthTexture.Sample(sampler_OtherEyeDepthTexture, otherEyeUV + float2(0, 2 / _ScreenParams.y), 0);
+				otherEyeDepth = max(otherEyeDepth, _OtherEyeDepthTexture.Sample(sampler_OtherEyeDepthTexture, otherEyeUV + float2(2 / _ScreenParams.x, -2 / _ScreenParams.y), 0));
+				otherEyeDepth = max(otherEyeDepth, _OtherEyeDepthTexture.Sample(sampler_OtherEyeDepthTexture, otherEyeUV + float2(-2 / _ScreenParams.x, -2 / _ScreenParams.y), 0));
+				return otherEyeDepth;
+			}
+
+			float4 frag(v2f i) : SV_Target
+			{
+				// read depth and reconstruct world position of pixel
+				float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv);
+				float4 clipSpacePos = float4(i.uv*2.0 - 1.0, depth, 1);
+				float4 worldPos = mul(_InvViewProj, clipSpacePos);
+				worldPos.xyz /= worldPos.w;
+
+				// transform into other eye's view
+				float4 otherClipSpacePos = mul(_OtherEyeViewProj, float4(worldPos.xyz, 1));
+				otherClipSpacePos.xyz /= otherClipSpacePos.w;
+
+				// read color
+				float2 otherEyeUV = otherClipSpacePos.xy*0.5 + 0.5;
+				float4 otherEyeColor = _OtherEyeTex.Sample(sampler_OtherEyeTex, otherEyeUV, 0);
+				float isReprojectable = otherEyeColor.a;
+
+				// Verify if if it is a valid reprojection by using conservativeFilter
+				float otherEyeDepth = GetOtherEyeConservativeDepth(otherEyeUV);
+				float diff = otherEyeDepth - depth;
+				if (diff > _DepthThreshold * isReprojectable)
+				{
+					discard;            // this will be filled in later
+				}
+				
+				return _ReprojectionMaskColor * otherEyeColor;
+			 }
+			 ENDCG
+		 }
+	}
+}
---
+++ VRJam/VRJam/Assets/Script/GlobalReprojectionManager.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.VR;
+
+#if UNITY_EDITOR
+using UnityEditor;
+[InitializeOnLoad]
+public class ReprojectionCompatibilityCheck
+{
+	static ReprojectionCompatibilityCheck()
+	{
+		EditorApplication.update += Update;
+	}
+
+	static void Update()
+	{
+#if UNITY_5_6_OR_NEWER
+		if (PlayerSettings.stereoRenderingPath != StereoRenderingPath.MultiPass)
+			Debug.LogError("The Reprojection Demo is ONLY supported under MultiPass stereo rendering path ");
+
+		BuildTargetGroup target = EditorUserBuildSettings.selectedBuildTargetGroup;
+		for (int tier = (int)UnityEngine.Rendering.GraphicsTier.Tier1; tier <= (int)UnityEngine.Rendering.GraphicsTier.Tier3; tier++)
+		{
+			var tierSettings = UnityEditor.Rendering.EditorGraphicsSettings.GetTierSettings(target, (UnityEngine.Rendering.GraphicsTier)tier);
+			if (tierSettings.renderingPath != RenderingPath.Forward)
+				Debug.LogError("The Reprojection Demo is implemented under forward renderer only");
+		}
+#else
+		Debug.LogError("The Reprojection Demo is implemented under Unity 5.6 or newer versions ");
+#endif
+	}
+}
+#endif
+
+
+[RequireComponent(typeof(Camera))]
+public class GlobalReprojectionManager : MonoBehaviour
+{
+	private Camera ownerCam;
+	private RenderTexture renderTex = null;
+	private RenderTexture depthTex;
+
+	private RenderTextureFormat format;
+	private int depthBits = 24;
+
+	public bool allowReprojection;
+	public bool showReprojectionMask;
+
+	private Material copyDepthMaterial;
+	private Material reprojectionPassMaterial;
+	private Material depthRestoreMaterial;
+
+	// Use this for initialization
+	void OnEnable()
+	{
+		// By default, Unity 5.6 is using ARGBHalf
+		format = RenderTextureFormat.ARGBHalf;
+
+		// Make sure the following shaders inside your resources folder or being referenced
+		if (copyDepthMaterial == null)
+			copyDepthMaterial = new Material(Shader.Find("Custom/Copy Depth"));
+
+		if (reprojectionPassMaterial == null)
+			reprojectionPassMaterial = new Material(Shader.Find("Custom/GlobalReprojectionPass"));
+
+		if (depthRestoreMaterial == null)
+			depthRestoreMaterial = new Material(Shader.Find("Custom/Depth Restore"));
+
+		ownerCam = (Camera)GetComponent<Camera>();
+		ownerCam.depthTextureMode = DepthTextureMode.Depth;
+		CreateRenderTex();
+		BuildCommandBuffers(ownerCam);
+	}
+
+	// Get matrix to transform from tracking space to world space 
+	// Given the facts we know both head camera's world transform information and tracking space information
+	static Matrix4x4 TrackingSpaceToWorldSpace(Camera headCamera)
+	{
+		Vector3 headPosition = UnityEngine.XR.InputTracking.GetLocalPosition(UnityEngine.XR.XRNode.Head);
+		Quaternion headOrientation = UnityEngine.XR.InputTracking.GetLocalRotation(UnityEngine.XR.XRNode.Head);
+		Matrix4x4 headPoseInTrackingSpace = Matrix4x4.TRS(headPosition, headOrientation, Vector3.one);
+
+		// Transform from tracking-Space to head-Space
+		Matrix4x4 trackingSpaceToHeadSpace = headPoseInTrackingSpace.inverse;
+
+		// Transform from head space to world space
+		Matrix4x4 ret = headCamera.transform.localToWorldMatrix * trackingSpaceToHeadSpace;
+		return ret;
+	}
+
+	// Get specified eye's world to camera matrix
+	Matrix4x4 GetVRWorldToCameraMatrix(Camera cam, UnityEngine.XR.XRNode eye)
+	{
+		Vector3 pos = UnityEngine.XR.InputTracking.GetLocalPosition(eye);
+		Quaternion rot = UnityEngine.XR.InputTracking.GetLocalRotation(eye);
+		Matrix4x4 m = Matrix4x4.TRS(pos, rot, Vector3.one);
+		m = TrackingSpaceToWorldSpace(cam) * m;
+		m = Matrix4x4.Inverse(m);
+
+		// Unity uses OpenGL convention, forward is -Z
+		// http://docs.unity3d.com/ScriptReference/Camera-worldToCameraMatrix.html
+		// so negate z row
+
+		m.m20 *= -1f;
+		m.m21 *= -1f;
+		m.m22 *= -1f;
+		m.m23 *= -1f;
+
+		return m;
+	}
+
+
+	// Get specified eye's world to view projection matrix
+	Matrix4x4 CalcViewProjection(Camera cam, UnityEngine.XR.XRNode eye)
+	{
+		Matrix4x4 viewMat = GetVRWorldToCameraMatrix(cam, eye);
+		Matrix4x4 projMat = GL.GetGPUProjectionMatrix(cam.projectionMatrix, true);
+		return projMat * viewMat;
+	}
+
+
+	void CreateRenderTex()
+	{
+		int width = (int)GetComponent<Camera>().pixelWidth;
+		int height = (int)GetComponent<Camera>().pixelHeight;
+		if (renderTex == null)
+		{
+			renderTex = new RenderTexture(width, height, depthBits, format);
+			renderTex.Create();
+			Debug.Log("Render texture size: " + renderTex.width + ", " + renderTex.height);
+			renderTex.hideFlags = HideFlags.DontSave;
+		}
+
+		if (depthTex == null)
+		{
+			// Need this because shader can't read from Unity RenderBuffer:
+			depthTex = new RenderTexture(width, height, 0, RenderTextureFormat.RFloat);
+			depthTex.filterMode = FilterMode.Point;
+			depthTex.Create();
+			depthTex.hideFlags = HideFlags.DontSave;
+		}
+	}
+
+	// Create command buffer for frame buffer copying
+	private CommandBuffer bufferSavingCommands;
+	private CommandBuffer depthRestoreCommands;
+	private CommandBuffer reprojectionCommands;
+
+	private void BuildCommandBuffers(Camera targetCamera)
+	{
+		if (bufferSavingCommands == null)
+		{
+			bufferSavingCommands = new CommandBuffer();
+			bufferSavingCommands.name = "Blit Left eye depth and color";
+			bufferSavingCommands.Blit(BuiltinRenderTextureType.CurrentActive, renderTex);
+			bufferSavingCommands.Blit(BuiltinRenderTextureType.Depth, depthTex, copyDepthMaterial);
+		}
+
+		if (reprojectionCommands == null)
+		{
+			reprojectionCommands = new CommandBuffer();
+			reprojectionCommands.name = "Reprojection Global Pass";
+			reprojectionCommands.SetGlobalTexture("_OtherEyeTex", renderTex);
+			reprojectionCommands.SetGlobalTexture("_OtherEyeDepthTexture", depthTex);
+			reprojectionCommands.Blit(BuiltinRenderTextureType.None, BuiltinRenderTextureType.CameraTarget, reprojectionPassMaterial);
+		}
+
+		if (depthRestoreCommands == null)
+		{
+			depthRestoreCommands = new CommandBuffer();
+			depthRestoreCommands.name = "Depth Restore Pass";
+			depthRestoreCommands.Blit(BuiltinRenderTextureType.None, BuiltinRenderTextureType.CameraTarget, depthRestoreMaterial);
+		}
+	}
+
+	void OnPreRender()
+	{
+		if (ownerCam && allowReprojection)
+		{
+			if (ownerCam.stereoActiveEye == Camera.MonoOrStereoscopicEye.Right)
+			{
+				reprojectionCommands.SetGlobalMatrix("_OtherEyeViewProj", CalcViewProjection(ownerCam, UnityEngine.XR.XRNode.LeftEye));
+				reprojectionCommands.SetGlobalMatrix("_InvViewProj", CalcViewProjection(ownerCam, UnityEngine.XR.XRNode.RightEye).inverse);
+				ownerCam.AddCommandBuffer(CameraEvent.BeforeForwardOpaque, reprojectionCommands);
+				ownerCam.AddCommandBuffer(CameraEvent.AfterForwardOpaque, depthRestoreCommands);
+			}
+			else if (ownerCam.stereoActiveEye == Camera.MonoOrStereoscopicEye.Left)
+			{
+				ownerCam.AddCommandBuffer(CameraEvent.AfterForwardOpaque, bufferSavingCommands);
+			}
+		}
+	}
+
+	void OnPostRender()
+	{
+		ownerCam.RemoveAllCommandBuffers();
+	}
+}
+
